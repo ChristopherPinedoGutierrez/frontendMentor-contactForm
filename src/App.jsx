dfa57117ac48813ae34737641522959cc5b6c7ef
Christopher Pinedo <cdpg.dev@gmail.com>
2025-01-11 08:47:04 -0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">
+          ¡Hola, Tailwind con React y Vite!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
